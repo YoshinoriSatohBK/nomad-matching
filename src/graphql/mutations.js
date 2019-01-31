5454e251f42779d607739aa4f0716e23b3a3c7ce
yoshinori satoh <yoshinori.satoh.tokyo@gmail.com>
2019-01-31 14:13:16 +0900

    first commit

---
+++ src/graphql/mutations.js
@@ -0,0 +1,51 @@
+// eslint-disable
+// this is an auto generated file. This will be overwritten
+
+export const createTask = `mutation CreateTask($input: CreateTaskInput!) {
+  createTask(input: $input) {
+    id
+    title
+    description
+    status
+  }
+}
+`;
+export const updateTask = `mutation UpdateTask($input: UpdateTaskInput!) {
+  updateTask(input: $input) {
+    id
+    title
+    description
+    status
+  }
+}
+`;
+export const deleteTask = `mutation DeleteTask($input: DeleteTaskInput!) {
+  deleteTask(input: $input) {
+    id
+    title
+    description
+    status
+  }
+}
+`;
+export const createPrivateNote = `mutation CreatePrivateNote($input: CreatePrivateNoteInput!) {
+  createPrivateNote(input: $input) {
+    id
+    content
+  }
+}
+`;
+export const updatePrivateNote = `mutation UpdatePrivateNote($input: UpdatePrivateNoteInput!) {
+  updatePrivateNote(input: $input) {
+    id
+    content
+  }
+}
+`;
+export const deletePrivateNote = `mutation DeletePrivateNote($input: DeletePrivateNoteInput!) {
+  deletePrivateNote(input: $input) {
+    id
+    content
+  }
+}
+`;
---
+++ src/graphql/subscriptions.js
@@ -0,0 +1,51 @@
+// eslint-disable
+// this is an auto generated file. This will be overwritten
+
+export const onCreateTask = `subscription OnCreateTask {
+  onCreateTask {
+    id
+    title
+    description
+    status
+  }
+}
+`;
+export const onUpdateTask = `subscription OnUpdateTask {
+  onUpdateTask {
+    id
+    title
+    description
+    status
+  }
+}
+`;
+export const onDeleteTask = `subscription OnDeleteTask {
+  onDeleteTask {
+    id
+    title
+    description
+    status
+  }
+}
+`;
+export const onCreatePrivateNote = `subscription OnCreatePrivateNote {
+  onCreatePrivateNote {
+    id
+    content
+  }
+}
+`;
+export const onUpdatePrivateNote = `subscription OnUpdatePrivateNote {
+  onUpdatePrivateNote {
+    id
+    content
+  }
+}
+`;
+export const onDeletePrivateNote = `subscription OnDeletePrivateNote {
+  onDeletePrivateNote {
+    id
+    content
+  }
+}
+`;
