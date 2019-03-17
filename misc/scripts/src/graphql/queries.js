4bd8a1619e9dcbc9aa8870741b0a12785ad393d9
yoshinori satoh <yoshinori.satoh.tokyo@gmail.com>
2019-03-17 14:42:20 +0900

    メンテナンス対応

---
+++ misc/scripts/src/graphql/queries.js
@@ -0,0 +1,74 @@
+// eslint-disable
+// this is an auto generated file. This will be overwritten
+
+export const getUserProfile = `query GetUserProfile($id: ID!) {
+  getUserProfile(id: $id) {
+    id
+    name
+    email
+    description
+    twitterScreenName
+    imageUrl
+    location
+    income
+    skill
+    smoking
+    drink
+    nomadStatus
+  }
+}
+`;
+export const listUserProfiles = `query ListUserProfiles(
+  $filter: ModelUserProfileFilterInput
+  $limit: Int
+  $nextToken: String
+) {
+  listUserProfiles(filter: $filter, limit: $limit, nextToken: $nextToken) {
+    items {
+      id
+      name
+      email
+      description
+      twitterScreenName
+      imageUrl
+      location
+      income
+      skill
+      smoking
+      drink
+      nomadStatus
+    }
+    nextToken
+  }
+}
+`;
+export const searchUserProfiles = `query SearchUserProfiles(
+  $filter: SearchableUserProfileFilterInput
+  $sort: SearchableUserProfileSortInput
+  $limit: Int
+  $nextToken: Int
+) {
+  searchUserProfiles(
+    filter: $filter
+    sort: $sort
+    limit: $limit
+    nextToken: $nextToken
+  ) {
+    items {
+      id
+      name
+      email
+      description
+      twitterScreenName
+      imageUrl
+      location
+      income
+      skill
+      smoking
+      drink
+      nomadStatus
+    }
+    nextToken
+  }
+}
+`;
---
+++ misc/scripts/src/graphql/subscriptions.js
@@ -0,0 +1,54 @@
+// eslint-disable
+// this is an auto generated file. This will be overwritten
+
+export const onCreateUserProfile = `subscription OnCreateUserProfile {
+  onCreateUserProfile {
+    id
+    name
+    email
+    description
+    twitterScreenName
+    imageUrl
+    location
+    income
+    skill
+    smoking
+    drink
+    nomadStatus
+  }
+}
+`;
+export const onUpdateUserProfile = `subscription OnUpdateUserProfile {
+  onUpdateUserProfile {
+    id
+    name
+    email
+    description
+    twitterScreenName
+    imageUrl
+    location
+    income
+    skill
+    smoking
+    drink
+    nomadStatus
+  }
+}
+`;
+export const onDeleteUserProfile = `subscription OnDeleteUserProfile {
+  onDeleteUserProfile {
+    id
+    name
+    email
+    description
+    twitterScreenName
+    imageUrl
+    location
+    income
+    skill
+    smoking
+    drink
+    nomadStatus
+  }
+}
+`;
